dcfbd96812d936b0b02e74636feecbaaa0b943fb
vrajdesai78 <vrajdesai78@gmail.com>
2023-01-15 21:21:19 +0530

    Added logo in navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,9 +2,11 @@ import {
     chakra,
     Flex,
     HStack,
+    Text
 } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
-import Image from 'next/image';
+import { useRouter } from "next/router";
+import Image from "next/image"
 import React from "react";
 import logo from '../assets/logo.png';
 
@@ -18,16 +20,20 @@ export const NavBar = () => {
         { ssr: false }
     );
 
+    const router = useRouter();
+
     return (
         <React.Fragment>
-            <chakra.header
+            <chakra.header  
                 bg={'transparent'}
                 w="full"
                 px={{ base: 4, sm: 6 }}
                 py={4}
             >
                 <Flex alignItems="center" justifyContent="space-between" mx="auto">
-                    <Flex>
+                    <Flex padding="1" border={'1px'} borderRadius="1em" onClick={() => {router.push('/')}} cursor={'pointer'} background="#dae3fb91">
+                        <Image src={logo} alt={'logo'} width={'50'}/>
+                        <Text alignSelf={'center'} fontSize="md" fontWeight={'bold'} textColor="#260367" paddingEnd={'0.5em'}>Buy Me A Glass</Text>
                     </Flex>
                     <HStack display="flex" alignItems="center" spacing={1} >
                         <WalletMultiButtonDynamic style={{ 'background':'#260367' }} />

e414b0132a58443aa00aee5779f7a8c66681a6a5
vrajdesai78 <vrajdesai78@gmail.com>
2023-03-06 21:57:36 +0530

    Removed unnecessary files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { ContextProvider } from "../contexts/ContextProvider";
-import { FC } from 'react';
+import { FC, useMemo } from "react";
 import type { AppProps } from "next/app";
-require("@solana/wallet-adapter-react-ui/styles.css");
-require("../styles/globals.css");
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider,
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
 
-// Change the network to the one you want to use: "mainnet-beta", "testnet", "devnet", "localhost" or your own RPC endpoint
+require('@solana/wallet-adapter-react-ui/styles.css');
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   const colors = {
@@ -16,15 +20,30 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
     },
   };
 
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+      () => [
+          new PhantomWalletAdapter(),
+      ],
+      [network]
+  );
+
   const theme = extendTheme({ colors });
 
   return (
-    <ContextProvider>
-      <ChakraProvider theme={theme}>
-          <Component {...pageProps} />
-      </ChakraProvider>
-    </ContextProvider>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+            </ChakraProvider>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
   );
-}
+};
 
 export default App;
